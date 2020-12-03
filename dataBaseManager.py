af47b315e9990958e3179514fb5651ddc1c4e804
kdator <colpakov.dmitry@gmail.com>
2020-12-03 10:07:18 +0300

    DBM: Offer's implementation.

--- dataBaseManager.py
+++ dataBaseManager.py
@@ -15,7 +15,6 @@ from models import User, Offer, Assessment
 
 database = None  # Используемая база данных
 cursor = None    # Объект взаимодействия с database
-databaseName = 'truejob_database.db'
 
 
 """
@@ -32,7 +31,7 @@ def connect():
   """
   global database
   global cursor
-  database = sqlite3.connect('truejob_database.db')
+  database = sqlite3.connect('truejob_database.db', check_same_thread=False)
   cursor = database.cursor()
   if database is None or cursor is None:
     return False
@@ -68,18 +67,16 @@ def addUser(user: User):
   """
   Добавить пользователя в бд
 
-  return: True добавление пользователя с новым vkid успешно, иначе False 
+  return: True добавление успешно, иначе False 
   """
-  with sqlite3.connect(databaseName) as db:
-    cursor = db.cursor()
-    cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (user.vkid, ))
-    if cursor.fetchone() is None:
-      cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
-                    (user.vkid, user.type, user.employer_rating,
-                      user.worker_rating, user.status, user.is_blocked))
-      db.commit()
-      return True
-    return False
+  cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (user.vkid, ))
+  if cursor.fetchone() is None:
+    cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
+                   (user.vkid, user.type, user.employer_rating,
+                    user.worker_rating, user.status, user.is_blocked))
+    database.commit()
+    return True
+  return False
 
 
 def updateUser(user: User):
@@ -88,24 +85,19 @@ def updateUser(user: User):
 
   return: True пользователь есть в бд и успешно обновлен, иначе False 
   """
-  with sqlite3.connect(databaseName) as db:
-    cursor = db.cursor()
-    cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (user.vkid, ))
-    if cursor.fetchone() is not None:
-      cursor.execute("UPDATE users SET \
-          vkid = (?), \
-          type = (?), \
-          employer_rating = (?), \
-          worker_rating = (?), \
-          status = (?), \
-          is_blocked = (?) \
-          WHERE vkid = (?)",
-          (user.vkid, user.type, user.employer_rating,
-          user.worker_rating, user.status, user.is_blocked,
-          user.vkid))
-      db.commit()
-      return True
-    return False
+  cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (user.vkid, ))
+  if cursor.fetchone() is not None:
+    cursor.execute("UPDATE users SET \
+        vkid = (?), type = (?), \
+        employer_rating = (?), worker_rating = (?), \
+        status = (?), is_blocked = (?) \
+        WHERE vkid = (?)",
+        (user.vkid, user.type, user.employer_rating,
+         user.worker_rating, user.status, user.is_blocked,
+         user.vkid))
+    database.commit()
+    return True
+  return False
 
 
 def getUser(vkid: str):
@@ -114,23 +106,20 @@ def getUser(vkid: str):
 
   return:  User с id == vkid, иначе None
   """
-  with sqlite3.connect(databaseName) as db:
-    cursor = db.cursor()
-    user = None
-
-    cursor.execute("SELECT * FROM users WHERE vkid = (?)", (vkid, ))
-    row = cursor.fetchone()
-    if row is not None:
-      """
-      cursor.description возвращает имена столбцов из последнего запроса.
-      он возвращает кортеж из 7 значений, где последние 6 = None.
-      на нулевой позиции лежит название столбца, поэтому его и используем для
-      формирования словаря.
-      """
-      rowDict = dict(zip([column[0] for column in cursor.description], row))
-      print("User", rowDict)
-      user = User(**rowDict)
+  cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (user.vkid, ))
+  row = cursor.fetchone() # создаём строку из таблицы для удобства проверки 
+    # последующей конвертации строки таблицы в словарь.
+  if row is not None:
+    """
+    cursor.description возвращает имена столбцов из последнего запроса.
+    он возвращает кортеж из 7 значений, где последние 6 = None.
+    на нулевой позиции лежит название столбца, поэтому его и используем для
+    формирования словаря.
+    """
+    rowDict = dict(zip([column[0] for column in cursor.description], row))
+    user = User(rowDict)
     return user
+  return None
 
 
 def deleteUser(vkid: str):
@@ -139,14 +128,12 @@ def deleteUser(vkid: str):
 
   return: True, если успешно, иначе False
   """
-  with sqlite3.connect(databaseName) as db:
-    cursor = db.cursor()
-    cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (vkid, ))
-    if cursor.fetchone() is not None:
-      cursor.execute("DELETE FROM users WHERE vkid = (?)", (vkid, ))
-      database.commit()
-      return True
-    return False
+  cursor.execute("SELECT vkid FROM users WHERE vkid = (?)", (vkid, ))
+  if cursor.fetchone() is not None:
+    cursor.execute("DELETE FROM users WHERE vkid = (?)", (vkid, ))
+    database.commit()
+    return True
+  return False
 
 
 # Функции связанные с Offer
@@ -156,8 +143,17 @@ def addOffer(offer: Offer):
 
   return: True добавление успешно, иначе False 
   """
-
-  return False
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT offer_id FROM offers WHERE offer_id = (?)", (offer.offer_id, ))
+    if cursor.fetchone() is None:
+      cursor.execute("INSERT INTO offers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (offer.offer_id, offer.name, offer.description, offer.price, offer.exp_date,
+          offer.location, offer.status, offer.owner, offer.priority, offer.time_created,
+          offer.views_counter, offer.likes_counter))
+      database.commit()
+      return True
+    return False
 
 
 def updateOffer(offer: Offer):
@@ -166,18 +162,48 @@ def updateOffer(offer: Offer):
 
   return: True пользователь есть в бд и успешно обновлен, иначе False 
   """
-
-  return False
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT offer_id FROM offers WHERE offet_id = (?)", (offer.offer_id, ))
+    if cursor.fetchone() is not None:
+      cursor.execute("UPDATE offers SET \
+        offer_id = (?), name = (?), \
+        description = (?), price = (?), \
+        exp_date = (?), location = (?), \
+        status = (?), owner = (?), \
+        priority = (?), time_created = (?), \
+        views_counter = (?), likes_counter = (?) \
+        WHERE offer_id = (?)",
+        (offer.offer_id, offer.name, offer.description, offer.price, offer.exp_date,
+        offer.location, offer.status, offer.owner, offer.priority, offer.time_created,
+        offer.views_counter, offer.likes_counter, offer.offer_id))
+      database.commit()
+      return True
+    return False
 
 
 def getOffer(id: int):
   """
   Получить объявление по id
 
-  return:  User с id == vkid, иначе None
+  return:  Offer с offed_id == id, иначе None
   """
-
-  return None
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM offers WHERE offer_id = (?)", (id, ))
+    row = cursor.fetchone() # создаём строку из таблицы объявлений для удобства проверки 
+      # и последующей конвертации строки в словарь.
+    if row is not None:
+      """
+      cursor.description возвращает имена столбцов из последнего запроса.
+      он возвращает кортеж из 7 значений, где последние 6 = None.
+      на нулевой позиции лежит название столбца, поэтому его и используем для
+      формирования словаря.
+      """
+      rowDict = dict(zip([column[0] for column in cursor.description], row))
+      offer = Offer(**rowDict)
+      return offer
+    return None
 
 
 def deleteOffer(id: int):
@@ -186,8 +212,14 @@ def deleteOffer(id: int):
 
   return: True, если успешно, иначе False
   """
-
-  return False
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT offer_id FROM offers WHERE offer_id = (?)", (id, ))
+    if cursor.fetchone() is not None:
+      cursor.execute("DELETE FROM offers WHERE offer_id = (?)", (id, ))
+      database.commit()
+      return True
+    return False
 
 
 def getOffersByUser(vkid: str):
@@ -196,16 +228,23 @@ def getOffersByUser(vkid: str):
 
   return: [Offer, ...] если такие есть, иначе None
   """
-  req = "SELECT * FROM Offers WHERE owner=vkid"
-
-  return None
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM offers WHERE owner = (?)", (vkid, ))
+    rows = cursor.fetchall()
+    if rows is not None:
+      list_of_offers = list()
+      for row in rows:
+        list_of_offers.append(list(row))
+      return list_of_offers
+    return None
 
 
 def getLikedOffersByUser(vkid: str):
   """
   Получить лайкнутые пользователем с vkid объявления 
   """
-
+  
   req = "SELECT Offers.* FROM LikedOffers, Offers \
           WHERE LikedOffers.user = vkid \
           AND LikedOffers.offer_id = Offers.offer_id"
@@ -219,8 +258,13 @@ def getAllOffers():
 
   return: [Offer, ...] Все объявления, если есть, иначе None
   """
-
-  return [None]
+  with sqlite3.connect(databaseName) as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM offers")
+    list_of_offers = list(cursor.fetchall())
+    if list_of_offers is not None
+      return list_of_offers
+    return None
 
 
 # Функции связанные с Like & Report & Assessment
@@ -297,6 +341,8 @@ def getAllAssessments(assessed_vkid: str):
 #   cursor = database.cursor()
 #   for value in cursor.execute(f"SELECT * FROM users"):
 #     print(value)
+#   for offer in cursor.execute(f"SELECT * FROM offers"):
+#     print(offer)
 
 
 # if __name__ == "__main__":
@@ -306,17 +352,30 @@ def getAllAssessments(assessed_vkid: str):
 #             "worker_rating": 2.2, "status": "super", "is_blocked": 1}
 #   user_dict_3 = {"vkid": "_kr.alex_", "type": "admin", "employer_rating": 5.0,
 #             "worker_rating": 5.0, "status": "Gold", "is_blocked": 0}
+#   first_offer_json = {"offer_id": 123, "name": "first offer", "description": "stupido",
+#                  "price": 10030, "exp_date": "27.01.2021", "location": "Moscow",
+#                  "status": "active", "owner": "kdator", "priority": "regular",
+#                  "time_created": "26.11.2020", "views_counter": 2, "likes_counter": 3}
+#   second_offer_json = {"offer_id": 124, "name": "first offer", "description": "stupido",
+#                  "price": 10030, "exp_date": "27.01.2021", "location": "Moscow",
+#                  "status": "active", "owner": "kdator", "priority": "regular",
+#                  "time_created": "26.11.2020", "views_counter": 2, "likes_counter": 3}
 #   first_user = User(user_dict_1)
-#   second_user = User(user_dict_2)
 #   third_user = User(user_dict_3)
+#   first_offer = Offer(first_offer_json)
+#   second_offer = Offer(second_offer_json)
 #   initNewConnect()
 #   addUser(first_user)
 #   addUser(third_user)
+#   addOffer(first_offer)
+#   addOffer(second_offer)
 #   printResult()
 #   print("------------------------")
-#   updateUser(second_user)
 #   printResult()
-#   deleteUser("kdator")
+#   deleteUser("_kr.alex_")
 #   print("------------------------")
 #   printResult()
+#   list_of_offers = getOffersByUser("kdator")
+#   for offer in list_of_offers:
+#     print(offer)
 #   closeConnect()
