72ae82c3542d32c5633af49e650dae2884663c8b
kdator <colpakov.dmitry@gmail.com>
2020-12-16 19:27:46 +0300

    tests: added some tests.

--- tests/DataBaseManagerTests.py
+++ tests/DataBaseManagerTests.py
@@ -20,13 +20,31 @@ class DataBaseManagerTests(unittest.TestCase):
   def tearDown(self):
     self.database = DB.closeConnect()
 
-  def test_AddUser(self):
+  def test_01_addUser(self):
     user = User(**self.user_dict)
     self.assertTrue(DB.addUser(user))
 
-  def test_updateUser(self):
+  def test_02_updateUser(self):
     user = User(**self.user_dict_to_upd)
     self.assertTrue(DB.updateUser(user))
+
+  def test_03_deleteUser(self):
+    self.assertTrue(DB.deleteUser("kdator"))
+
+  @unittest.expectedFailure
+  def test_04_deleteFakeUser(self):
+    self.assertTrue(DB.deleteUser("__krax.__"))
+
+  def test_05_addOffer(self):
+    offer = Offer(self.offer_dict)
+    self.assertTrue(DB.addOffer(offer))
+
+  def test_06_deleteOffer(self):
+    self.assertTrue(DB.deleteOffer(1))
+
+  @unittest.expectedFailure
+  def test_07_deleteFakeOffer(self):
+    self.assertTrue(DB.deleteOffer(2))
 
 if __name__ == '__main__':
   unittest.main()
